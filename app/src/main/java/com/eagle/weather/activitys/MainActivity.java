927eaf99e2c6784cba1a4d772c9758ee6d00323b
ZoneLai <185838191@qq.com>
2017-12-06 23:16:23 +0800

    First commit

---
+++ app/src/main/java/com/eagle/weather/activitys/MainActivity.java
@@ -0,0 +1,15 @@
+package com.eagle.weather.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eagle.weather.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
